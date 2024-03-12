ba12c85e9b8effc2f87b77f0e060fd2a62f1a44d
Niki42 <nik.7377@gmail.com>
2024-03-12 18:14:43 +0100

    firefox?

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 def basic_test():
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get("http://localhost:5000")
     input_field = driver.find_element("id", "messageInput")
     assert input_field is not None
