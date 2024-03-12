573b1d87e59b49d08d9a49ff932e99dffe2c40ef
Niki42 <nik.7377@gmail.com>
2024-03-12 15:42:06 +0100

    flask test?

--- unit_test_split_msg.py
+++ unit_test_split_msg.py
@@ -1,11 +1,20 @@
 import client.split_msg as split_msg
 
-def test_example():
+def test_msg_split():
     msg = split_msg.splitMsg("")
 
     if msg.handelJoin(["niki@AfterLife JOIN #123"]) != "You joined #123\n":
         assert False
     assert True
 
+from webserver import app
+
+def test_flask_status_code():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+
 if __name__ == '__main__':
-    test_example()
+    test_msg_split()
+    test_flask_status_code()
