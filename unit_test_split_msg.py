0ec616c1ff993f8dd4accdc5bb440d8190c9959b
Niki42 <nik.7377@gmail.com>
2024-03-12 15:34:32 +0100

    add flask test?

--- unit_test_split_msg.py
+++ unit_test_split_msg.py
@@ -1,4 +1,10 @@
+import client.split_msg as split_msg
+
 def test_example():
+    msg = split_msg.splitMsg("")
+
+    if msg.handelJoin(["niki@AfterLife JOIN #123"]) != "You joined #123\n":
+        assert False
     assert True
 
 if __name__ == '__main__':
