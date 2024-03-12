1d1b9f8df4c94e947cc0b594b7c51c245c6ae2b4
Niki42 <nik.7377@gmail.com>
2024-03-12 19:10:46 +0100

    aaa

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -4,7 +4,7 @@ def test_example():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        page.goto('https://127.0.0.1:5000')
+        page.goto('http://127.0.0.1:5000')
         assert page.title() == 'AFTER LIFE'
         browser.close()
 
